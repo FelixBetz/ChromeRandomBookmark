34b165aacb1368ad9a45c60ce17839e48838b516
Felix Betz <betz.felix@web.de>
2022-10-11 14:12:35 +0200

    add api route: get bookmarks

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 """handels the flask backend"""
+
+
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, jsonify
 import chrome_bookmarks
 
 
@@ -8,12 +10,36 @@ app = Flask(__name__)
 cors = CORS(app)
 
 
-if __name__ == "__main__":
+@app.route("/api/bookmarks", methods=["GET"])
+def get_bookmarks():
+    """returns all bookmarks"""
 
+    ret_folders = []
     for folder in chrome_bookmarks.folders:
-        print(folder.name)
-        print(folder.folders)
-    for url in chrome_bookmarks.urls:
-        print(url.url, url.name)
+        element = {}
+        element["name"] = folder.name
+        url_ids = []
+        for url in folder.urls:
+            url_ids.append(int(url.id))
+        element["url_ids"] = url_ids
 
-    app.run(host="0.0.0.0", port=8080, debug=False)
+        ret_folders.append(element)
+
+    ret_urls = []
+    for url in chrome_bookmarks.urls:
+        element = {}
+        element["name"] = url.name
+        element["id"] = int(url.id)
+        element["url"] = url.url
+        element["date_added"] = url.added.timestamp()
+        try:
+            element["date_last_used"] = url.modified.timestamp()
+        except AttributeError:
+            element["date_last_used"] = 0
+        ret_urls.append(element)
+
+    return jsonify({"folders": ret_folders, "urls": ret_urls})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
