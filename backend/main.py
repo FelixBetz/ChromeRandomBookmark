aae30fd3ba4e4cac58d4f4cc9815630fbbbd93cd
Felix Betz <betz.felix@web.de>
2022-10-11 11:07:34 +0200

    read bookmarks and print them

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 """handels the flask backend"""
 from flask_cors import CORS
 from flask import Flask
+import chrome_bookmarks
+
 
 app = Flask(__name__)
 cors = CORS(app)
 
 
 if __name__ == "__main__":
+
+    for folder in chrome_bookmarks.folders:
+        print(folder.name)
+        print(folder.folders)
+    for url in chrome_bookmarks.urls:
+        print(url.url, url.name)
+
     app.run(host="0.0.0.0", port=8080, debug=False)
